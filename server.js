e14fb1815651de64fef8e2bbdf83d6f45a7ae6c0
Tolutronics <toluxy100@gmail.com>
2020-05-11 10:11:28 -0700

    deploy

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/dist/EduChild'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/EduChild/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
