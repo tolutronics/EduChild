79ffce6fa5d1897e558e7f3cedc3bedb37b3e76a
Tolutronics <toluxy100@gmail.com>
2020-05-15 06:00:04 -0700

    home-d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
-
+import {trigger, style, animate, transition, keyframes} from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  
+  animations: [
+    trigger('fade', [ 
+      transition('void => *', [
+        style({ opacity: 0 }), 
+        animate(5000, style({opacity: 1}))
+      ]) 
+    ])
+  ]
 })
 export class AppComponent{
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {trigger, style, animate, transition, keyframes} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fade', [ 
+      transition('void => *', [
+        style({ opacity: 0 }), 
+        animate(3000, style({opacity: 1}))
+      ]) 
+    ]),
+    trigger('bounce', [
+  
+      transition('void=> *', [
+        animate('3s', keyframes([
+          style({ transform: 'scale(1,1) translateY(0)' }),
+          style({ transform: 'scale(1.1, 0.9) translateY(0)' }),
+          style({ transform: 'scale(0.9, 1.1) translateY(-100px)' }),
+          style({ transform: 'scale(1.05, 0.95) translateY(0)' }),
+          style({ transform: 'scale(1,1) translateY(-7px)' }),
+          style({ transform: 'scale(1,1) translateY(0)' }),
+        ]))
+      ])
+    ])
+  ]
+
 })
 export class HomeComponent implements OnInit {
+  breakpoint: number;
+  public isMobile: boolean = false;
+  constructor(breakpointObserver: BreakpointObserver) {
 
-  constructor() { }
+    breakpointObserver.observe([
+      Breakpoints.Handset
+    ]).subscribe(result => {
+      this.isMobile = result.matches;
+    });
+   }
 
-  ngOnInit(): void {
-   
+  ngOnInit() {
+ 
+    
   }
+  
+
 
 }
